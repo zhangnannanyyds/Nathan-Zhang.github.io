bf2caeb5468b8dc602672991f9ea901657305eaf
zhangnannanyyds <zhangnannan005@gmail.com>
2026-09-15 10:41:25 +0800

    恢复首页

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
 // This file is intentionally blank
